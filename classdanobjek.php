d36c8c70884fef3f2e7cf452439c96711c051b33
Florecita Wenny <h1101241039@student.untan.ac.id>
2025-08-29 14:56:59 +0700

    Create classdanobjek.php

---
+++ classdanobjek.php
@@ -0,0 +1,11 @@
+<?php
+class persegipanjang {
+    // Property
+    public $panjang;
+    public $lebar;
+
+    // Method
+    public function jalan() {
+        return "Mobil $this->merk berwarna $this->warna sedang berjalan...";
+    }
+}
